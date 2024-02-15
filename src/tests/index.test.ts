8f40bfc08ef19b5a5bb3d2681b5465444f9ce3ca
XFernando43 <U20171E997@upc.edu.pe>
2024-02-15 16:07:59 -0500

    add testing

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,5 +1,19 @@
-// import { test,expect } from "vitest";
+import request from 'supertest'
+import { app } from "../app";
+import { test,expect } from "vitest";
 
-// test("get products",()=>{
-//     expect(true);
-// })
+
+test("get products",()=>{
+    expect(true);
+})
+
+test("Iniciar sesión de usuario", async () => {
+    const response = await request(app)
+      .post("/user/login")
+      .send({
+        email: 'Fernandorar65@gmail.com',
+        password: 'Mierdamierda123'
+    });
+    expect(response.status).toBe(200);
+    expect(response.header['content-type']).toMatch(/json/);
+});
