69259dfb0930866374195a3c77b3d39f3c7eb4eb
XFernando43 <U20171E997@upc.edu.pe>
2024-02-16 10:54:34 -0500

    add csv midelware geature

--- src/App/Routers/File.routes.ts
+++ src/App/Routers/File.routes.ts
@@ -3,13 +3,13 @@ import { File_Controller } from "../Controllers/File.controller";
 import { authenticateHandler } from "../../Midelware/authentication.mdw";
 import { authorize } from "../../Midelware/authorization.mdw";
 const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' }); // Directorio donde se guardarán temporalmente los archivos subidos
+const upload = multer({ dest: 'uploads/' }); 
 export const FileRouter = express.Router();
 
 FileRouter.post(
   "/upload",
-  upload.single('csvFile'),
   authenticateHandler,
   authorize("admin"),
+  upload.single('csvFile'),
   File_Controller.uploadCsv
 );
